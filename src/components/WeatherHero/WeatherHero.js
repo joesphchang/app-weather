0473c6e4f2cac38f8abe7679aab3e0fadd4cfd74
joesphchang <joesphschang@gmail.com>
2022-07-18 12:26:06 -0400

    scaffold out rest of the components for app

--- src/components/WeatherHero/WeatherHero.js
+++ src/components/WeatherHero/WeatherHero.js
@@ -1,28 +1,25 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect } from 'react';
 import './WeatherHero.css';
 
 function WeatherHero() {
+	const [currentWeather, setCurrentWeather] = useState([]);
 
-  const [currentWeather, setCurrentWeather] = useState([]);
+	const apiKey = process.env.REACT_APP_API;
 
-  const apiKey = process.env.REACT_APP_API;
+	useEffect(() => {
+		const url = `http://api.weatherapi.com/v1/current.json?key=${apiKey}&q=London&api=no`;
+		fetch(url)
+			.then((res) => res.json())
+			.then((res) => {
+				console.log(res.current);
+				setCurrentWeather(res);
+			})
+			.catch((err) => {
+				console.error(err);
+			});
+	}, []);
 
-  useEffect(() => {
-    const url = `http://api.weatherapi.com/v1/current.json?key=${apiKey}&q=London&api=no`;
-    fetch(url)
-      .then((res) => res.json())
-      .then((res) => {
-        console.log(res.current)
-        setCurrentWeather(res);
-      })
-      .catch((err) => {
-        console.error(err);
-      });
-  }, []);
-
-  return (
-    <div>WeatherHero</div>
-  )
+	return <div>WeatherHero</div>;
 }
 
-export default WeatherHero
+export default WeatherHero;
